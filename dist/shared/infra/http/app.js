0d87bd8ca65d541de26c909a6d69292a8891b4c9
felipebit <felipebit>
2021-02-17 23:16:17 -0300

    swagger removed

--- dist/shared/infra/http/app.js
+++ dist/shared/infra/http/app.js
@@ -1,23 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -27,10 +8,8 @@ require("express-async-errors");
 const dotenv_1 = __importDefault(require("dotenv"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const path_1 = __importDefault(require("path"));
-const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const cors_1 = __importDefault(require("cors"));
 const AppError_1 = __importDefault(require("@shared/errors/AppError"));
-const swaggerDocument = __importStar(require("@shared/infra/documentation/swagger.json"));
 const routes_1 = __importDefault(require("@shared/infra/http/routes"));
 dotenv_1.default.config();
 const { MONGO_ATLAS_USER, MONGO_ATLAS_PASSWORD, MONGO_ATLAS_DB } = process.env;
@@ -38,7 +17,6 @@ const mongoAtlasURI = `mongodb+srv://${MONGO_ATLAS_USER}:${MONGO_ATLAS_PASSWORD}
 mongoose_1.default.connect(mongoAtlasURI, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true }, () => console.log(" Mongoose is connected"));
 const app = express_1.default();
 app.use(express_1.default.static(path_1.default.join(__dirname, '../', 'public')));
-app.use('/swagger', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDocument));
 app.use(cors_1.default());
 app.use(express_1.default.json());
 app.use(routes_1.default);
