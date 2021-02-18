4489ac12d26cdc52d47f12e7d179bfb3e4441dea
felipebit <felipebit>
2021-02-18 15:40:52 -0300

    sending to AWS

--- src/config/amadeus.ts
+++ src/config/amadeus.ts
@@ -2,9 +2,11 @@ const Amadeus = require('amadeus');
 import dotenv from 'dotenv'
 dotenv.config()
 
+const { AMADEUS_CLIENT_ID, AMADEUS_CLIENT_SECRET } = process.env
+
 const amadeus = new Amadeus({
-    clientId: process.env.AMADEUS_CLIENT_ID,
-    clientSecret: process.env.AMADEUS_CLIENT_SECRET
+    clientId: AMADEUS_CLIENT_ID,
+    clientSecret: AMADEUS_CLIENT_SECRET
 });
 
 export default amadeus;
