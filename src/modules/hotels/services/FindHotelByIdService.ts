3915387c0925de264f883a20cf39dfa000dec68e
felipebit <felipebit>
2021-02-18 12:29:05 -0300

    release

--- src/modules/hotels/services/FindHotelByIdService.ts
+++ src/modules/hotels/services/FindHotelByIdService.ts
@@ -27,9 +27,21 @@ class FindHotelByIdService {
         }
     }
 
-    public async execute() {
+    private async accuweather(latlong: string) {
         const apikey = process.env.ACCUWEATHER_API_KEY
+        try {
+            const geopositionResult = await accuweather.get("locations/v1/cities/geoposition/search", { params: { apikey, q: latlong, language: 'EN', details: true, } })
+            const hotelLocationKey = geopositionResult.data.Details.CanonicalLocationKey || ''
 
+            const weatherResult = await accuweather.get(`currentconditions/v1/${hotelLocationKey}`, { params: { apikey, language: 'EN', details: true } })
+            this.hotelInfo.weather = weatherResult.data || ''
+        } catch (error) {
+            this.hotelInfo.weather = {}
+        }
+
+    }
+
+    public async execute() {
         const infoResult = await amadeus.shopping.hotelOffersByHotel.get({ hotelId: this.hotelId })
         this.hotelInfo.info = infoResult.data
 
@@ -37,21 +49,19 @@ class FindHotelByIdService {
         const longitude = infoResult.data.hotel.longitude
         const latlong = `${latitude},${longitude}`
 
-        //const geopositionResult = await accuweather.get("locations/v1/cities/geoposition/search", { params: { apikey, q: latlong, language: 'EN', details: true, } })
-        //const hotelLocationKey = geopositionResult.data.Details.CanonicalLocationKey || ''
-
         const activitiesResult = await amadeus.shopping.activities.get({ latitude, longitude, radius: 20 })
         this.hotelInfo.activities = activitiesResult.data
 
+        //await accuweather(latlong)
+
         //const locationsResult = await amadeus.referenceData.locations.pointsOfInterest.get({ latitude, longitude, radius: 10, categories: ['SIGHTS', 'NIGHTLIFE', 'RESTAURANT', 'SHOPPING'] })
         //this.hotelInfo.locations = locationsResult.data
-
-        //const weatherResult = await accuweather.get(`currentconditions/v1/${hotelLocationKey}`, { params: { apikey, language: 'EN', details: true } })
-        // this.hotelInfo.weather = weatherResult.data || ''
 
         return this.hotelInfo
 
 
     }
+
+
 }
 export default FindHotelByIdService
